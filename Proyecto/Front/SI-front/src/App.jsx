9a648bcab3a1aede883a78ea3d8c66a821559dba
ARES313 <jfigueroae@unal.edu.co>
2025-11-12 23:48:03 -0500

    Agregue metricas

--- Proyecto/Front/SI-front/src/App.jsx
+++ Proyecto/Front/SI-front/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PagLogin from './components/Pag_login.jsx';
 import Home from './Pages/Home.jsx';
 import Listas from './pages/Listas.jsx';
+import Metricas from './Pages/Metricas.jsx';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
         <Route path="/" element={<PagLogin />} />
         <Route path="/home" element={<Home />} />
         <Route path="/listas" element={<Listas />} />
+        <Route path="/metricas" element={<Metricas />} />
 
       </Routes>
     </BrowserRouter>
